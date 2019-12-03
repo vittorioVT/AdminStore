60853f30f5b42376d481753936dca687c0a92bd4
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-12-03 21:07:56 +0200

    Design table of products

---
+++ src/app/Interface/BodyElements.ts
@@ -0,0 +1,10 @@
+export interface BodyElements {
+  Id: number,
+  Name: string,
+  Description: string,
+  Picture: string,
+  Color: string,
+  Size: string,
+  Price: number,
+  Filter: string
+}
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BodyService } from '../body.service';
+import { MatTableDataSource } from '@angular/material';
+import { BodyElements } from '../Interface/BodyElements';
 
 @Component({
   selector: 'app-body',
@@ -9,11 +11,14 @@ import { BodyService } from '../body.service';
 export class BodyComponent implements OnInit {
 
   displayedColumns: string[] = ['Id', 'Name', 'Description', 'Picture', 'Color', 'Size', 'Price', 'Filter'];
+  dataSource;
   constructor(private service: BodyService) { }
 
   ngOnInit() {
     this.service.getAll().subscribe((data) => {
       console.log('Result - ', data);
+      this.dataSource = new MatTableDataSource<BodyElements>(data as BodyElements[]);
+
     })
 
   }
