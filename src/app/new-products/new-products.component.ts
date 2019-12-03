d7f892d5df4c0e2609ad5d5113b054caa8950e49
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-12-03 21:47:41 +0200

    Created Angular Reactive Form

--- src/app/new-products/new-products.component.ts
+++ src/app/new-products/new-products.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-new-products',
   templateUrl: './new-products.component.html',
   styleUrls: ['./new-products.component.css']
 })
-export class NewProductsComponent implements OnInit {
+export class NewProductsComponent {
 
   constructor() { }
+  productForm = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl(''),
+    description: new FormControl(''),
+    picture: new FormControl(''),
+    color: new FormControl(''),
+    size: new FormControl(''),
+    price: new FormControl(''),
+    filter: new FormControl('')
+  })
 
-  ngOnInit() {
-  }
 
+  
 }
