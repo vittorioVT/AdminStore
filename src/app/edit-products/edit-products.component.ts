2bc414bf7440ef480ac412799e3f8a2337c0863e
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-12-12 15:27:31 +0200

    Added routing for component edit

--- src/app/edit-products/edit-products.component.ts
+++ src/app/edit-products/edit-products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { BodyService } from '../body.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-products',
@@ -9,11 +10,13 @@ import { BodyService } from '../body.service';
   styleUrls: ['./edit-products.component.css']
 })
 export class EditProductsComponent implements OnInit {
+    [x: string]: any;
 
   form: FormGroup;
   id: number;
 
   constructor(private fb: FormBuilder,
+    private router: Router,
     private dialogRef: MatDialogRef<EditProductsComponent>,
     @Inject(MAT_DIALOG_DATA) { Name, Description, Picture, Color, Size, Price, Comment, Id },
     private service: BodyService) {
@@ -33,11 +36,13 @@ export class EditProductsComponent implements OnInit {
   }
 
   save() {
-    console.log('save clicked');
+    this.form.value.id = this.id;
     this.service.updateProduct(this.id, this.form.value).subscribe(
       (data) => {
         console.log('Data: ', data);
-      })
+      });
+    this.router.navigate(['/body']);
+    this.close();
   }
 
   close() {
